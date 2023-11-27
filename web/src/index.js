2fd89c733d978fc372274623757112abeec7638d
Andrew Karpow <andrew.karpow@sap.com>
2023-11-27 12:46:17 -0500

    [web] Move esbuild reload to index.js and use only in development env

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client"
 import React from "react"
 
+var NODE_ENV = process.env.NODE_ENV
+if (NODE_ENV === 'development') {
+  new EventSource('/esbuild').addEventListener('change', () => location.reload());
+}
+
 // export mount and unmount functions
 export const mount = (container, options = {}) => {
   import("./App").then((App) => {
