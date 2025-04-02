8cc768d403adf84935456e9de769ab3dd97e4980
Caio Ronchi <142912765+ronchi-oss@users.noreply.github.com>
2025-04-02 18:39:32 +0200

    renamed `quota.domain` to `quota.domain_akamai`

--- internal/client/quota.go
+++ internal/client/quota.go
@@ -43,11 +43,11 @@ type QuotaUpdate struct {
 	Positional struct {
 		ProjectID strfmt.UUID `description:"The ID of the project to update"`
 	} `positional-args:"yes" required:"yes"`
-	Domain     *int64 `long:"domain" description:"Domains integer value"`
-	Datacenter *int64 `long:"datacenter" description:"Datacenter integer value"`
-	Pool       *int64 `long:"pool" description:"Pool integer value"`
-	Member     *int64 `long:"member" description:"Member integer value"`
-	Monitor    *int64 `long:"monitor" description:"Monitor integer value"`
+	DomainAkamai *int64 `long:"domain_akamai" description:"Domains (provider Akamai) integer value"`
+	Datacenter   *int64 `long:"datacenter" description:"Datacenter integer value"`
+	Pool         *int64 `long:"pool" description:"Pool integer value"`
+	Member       *int64 `long:"member" description:"Member integer value"`
+	Monitor      *int64 `long:"monitor" description:"Monitor integer value"`
 }
 type QuotaDelete struct {
 	Positional struct {
@@ -61,9 +61,9 @@ func (*QuotaList) Execute(_ []string) error {
 		return err
 	}
 
-	Table.AppendHeader(table.Row{"Project ID", "Domains", "Datacenters", "Pools", "Members", "Monitors"})
+	Table.AppendHeader(table.Row{"Project ID", "DomainsAkamai", "Datacenters", "Pools", "Members", "Monitors"})
 	for _, quota := range resp.Payload.Quotas {
-		Table.AppendRow(table.Row{*quota.ProjectID, *quota.Domain, *quota.Datacenter, *quota.Pool,
+		Table.AppendRow(table.Row{*quota.ProjectID, *quota.DomainAkamai, *quota.Datacenter, *quota.Pool,
 			*quota.Member, *quota.Monitor})
 	}
 	Table.Render()
@@ -76,13 +76,13 @@ func (*QuotaDefaults) Execute(_ []string) error {
 		return err
 	}
 
-	Table.AppendHeader(table.Row{"Domains", "Datacenters", "Pools", "Members", "Monitors"})
-	domains := int(*resp.Payload.Quota.Domain)
+	Table.AppendHeader(table.Row{"DomainsAkamai", "Datacenters", "Pools", "Members", "Monitors"})
+	domains_akamai := int(*resp.Payload.Quota.DomainAkamai)
 	datacenters := int(*resp.Payload.Quota.Datacenter)
 	pools := int(*resp.Payload.Quota.Pool)
 	members := int(*resp.Payload.Quota.Member)
 	monitors := int(*resp.Payload.Quota.Monitor)
-	Table.AppendRow(table.Row{domains, datacenters, pools, members, monitors})
+	Table.AppendRow(table.Row{domains_akamai, datacenters, pools, members, monitors})
 	Table.Render()
 	return nil
 }
@@ -104,11 +104,11 @@ func (*QuotaUpdate) Execute(_ []string) error {
 		WithProjectID(QuotaOptions.QuotaUpdate.Positional.ProjectID.String()).
 		WithQuota(administrative.PutQuotasProjectIDBody{
 			Quota: &models.Quota{
-				Datacenter: QuotaOptions.QuotaUpdate.Datacenter,
-				Domain:     QuotaOptions.QuotaUpdate.Domain,
-				Member:     QuotaOptions.QuotaUpdate.Member,
-				Monitor:    QuotaOptions.QuotaUpdate.Monitor,
-				Pool:       QuotaOptions.QuotaUpdate.Pool,
+				Datacenter:   QuotaOptions.QuotaUpdate.Datacenter,
+				DomainAkamai: QuotaOptions.QuotaUpdate.DomainAkamai,
+				Member:       QuotaOptions.QuotaUpdate.Member,
+				Monitor:      QuotaOptions.QuotaUpdate.Monitor,
+				Pool:         QuotaOptions.QuotaUpdate.Pool,
 			},
 		})
 
