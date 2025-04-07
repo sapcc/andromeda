1e1a92ab3cb5cb62046f71b109d2017a16a20903
Andrew Karpow <andrew.karpow@sap.com>
2025-04-07 16:02:44 +0200

    [akamai] fix stormrpc recoverer middleware
    The recoverer returned a nil value when a panic has been captured,
    but didn't returned an error which could cause a segmentation fault in
    later processing.
    
    This changes ensure any panic is captures by sentry, but handled as an
    error by stormrpc.

--- middlewares/recoverer.go
+++ middlewares/recoverer.go
@@ -18,6 +18,7 @@ package middlewares
 
 import (
 	"context"
+	"time"
 
 	"github.com/actatum/stormrpc"
 	"github.com/getsentry/sentry-go"
@@ -26,7 +27,21 @@ import (
 // Recoverer is a stormrpc middleware that recovers from panics and sends them to Sentry.
 func Recoverer(next stormrpc.HandlerFunc) stormrpc.HandlerFunc {
 	return func(ctx context.Context, r stormrpc.Request) (resp stormrpc.Response) {
-		defer sentry.Recover()
+		defer func() {
+			err := recover()
+			if err != nil {
+				// Log the panic to Sentry
+				sentry.CurrentHub().RecoverWithContext(ctx, err)
+				sentry.Flush(time.Second * 5)
+
+				// pass back to the caller
+				resp = stormrpc.NewErrorResponse(
+					r.Reply,
+					stormrpc.Errorf(stormrpc.ErrorCodeInternal, "%v", err),
+				)
+			}
+		}()
+
 		resp = next(ctx, r)
 
 		return resp
