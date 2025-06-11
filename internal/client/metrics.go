1a9040d1f8694292ffc14e2ef9fba20a879929c5
Andrew Karpow <andy@ndyk.de>
2025-06-11 12:12:07 -0400

    Revert "following the correct pattern used in domain.go"
    This reverts commit b2ab7f49c1f8af7e02789f54f6ecfe94ed2ecd14.

--- internal/client/metrics.go
+++ internal/client/metrics.go
@@ -5,9 +5,14 @@
 package client
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/sapcc/andromeda/client/metrics"
+	"github.com/actatum/stormrpc"
+	"github.com/nats-io/nats.go"
+
+	"github.com/sapcc/andromeda/internal/config"
+	"github.com/sapcc/andromeda/models"
 )
 
 // CLI command options
@@ -23,28 +28,88 @@ type MetricsAkamaiDNS struct {
 
 // Execute the metrics command
 func (*MetricsAkamaiDNS) Execute(_ []string) error {
-	// Create params for the API call
-	params := metrics.NewGetMetricsAkamaiTotalDNSRequestsParams()
+	// Create metrics client
+	client, err := NewMetricsClient()
+	if err != nil {
+		return fmt.Errorf("failed to create metrics client: %w", err)
+	}
 
-	// Set request parameters if provided
+	var propName, projID, timeRange *string
 	if MetricsOptions.MetricsAkamaiDNS.PropertyName != "" {
-		params.PropertyName = &MetricsOptions.MetricsAkamaiDNS.PropertyName
+		propName = &MetricsOptions.MetricsAkamaiDNS.PropertyName
 	}
 	if MetricsOptions.MetricsAkamaiDNS.ProjectID != "" {
-		params.ProjectID = &MetricsOptions.MetricsAkamaiDNS.ProjectID
+		projID = &MetricsOptions.MetricsAkamaiDNS.ProjectID
 	}
 	if MetricsOptions.MetricsAkamaiDNS.TimeRange != "" {
-		params.TimeRange = &MetricsOptions.MetricsAkamaiDNS.TimeRange
+		timeRange = &MetricsOptions.MetricsAkamaiDNS.TimeRange
 	}
 
-	// Call the Andromeda API endpoint
-	resp, err := AndromedaClient.Metrics.GetMetricsAkamaiTotalDNSRequests(params)
+	result, err := client.GetAkamaiTotalDNSRequests(context.Background(), propName, projID, timeRange)
 	if err != nil {
 		return fmt.Errorf("failed to get DNS metrics: %w", err)
 	}
 
 	// Write result to table
-	return WriteTable(resp.Payload.TotalDNSRequests)
+	return WriteTable(result)
+}
+
+// MetricsClient provides access to metrics-related functionality
+type MetricsClient struct {
+	rpc *stormrpc.Client
+}
+
+// NewMetricsClient creates a new client for metrics operations
+func NewMetricsClient() (*MetricsClient, error) {
+	// Connect to NATS
+	nc, err := nats.Connect(config.Global.Default.TransportURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to NATS: %w", err)
+	}
+
+	// Create RPC client
+	rpcClient, err := stormrpc.NewClient("", stormrpc.WithNatsConn(nc))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create RPC client: %w", err)
+	}
+
+	return &MetricsClient{
+		rpc: rpcClient,
+	}, nil
+}
+
+// GetAkamaiTotalDNSRequests retrieves total DNS request metrics from Akamai GTM
+func (c *MetricsClient) GetAkamaiTotalDNSRequests(ctx context.Context, propertyName, projectID, timeRange *string) (*models.AkamaiTotalDNSRequests, error) {
+	// Prepare request parameters
+	params := struct {
+		PropertyName *string `json:"property_name,omitempty"`
+		ProjectID    *string `json:"project_id,omitempty"`
+		TimeRange    *string `json:"time_range,omitempty"`
+	}{
+		PropertyName: propertyName,
+		ProjectID:    projectID,
+		TimeRange:    timeRange,
+	}
+
+	// Create request
+	req, err := stormrpc.NewRequest("andromeda.get_dns_metrics.akamai", params)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create RPC request: %w", err)
+	}
+
+	// Send request
+	resp := c.rpc.Do(ctx, req)
+	if resp.Err != nil {
+		return nil, fmt.Errorf("RPC request failed: %w", resp.Err)
+	}
+
+	// Parse response
+	var result models.AkamaiTotalDNSRequests
+	if err := resp.Decode(&result); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return &result, nil
 }
 
 // Initialize the command
