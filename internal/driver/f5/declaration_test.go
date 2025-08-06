4c55c0a2181c4445f2acf30b78c9b058da51a3ee
Caio Ronchi <142912765+ronchi-oss@users.noreply.github.com>
2025-08-06 17:03:11 +0200

    declaration test

--- internal/driver/f5/declaration_test.go
+++ internal/driver/f5/declaration_test.go
@@ -7,11 +7,62 @@ package f5
 import (
 	"testing"
 
+	"github.com/sapcc/andromeda/internal/driver/f5/as3"
+	"github.com/sapcc/andromeda/internal/rpcmodels"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestBuildsA33Declaration(t *testing.T) {
+type mockedStore struct {
+	mock.Mock
+}
+
+func (s *mockedStore) GetDatacenters() ([]*rpcmodels.Datacenter, error) {
+	args := s.Called()
+	return args.Get(0).([]*rpcmodels.Datacenter), args.Error(1)
+}
+
+func (s *mockedStore) GetDomains() ([]*rpcmodels.Domain, error) {
+	args := s.Called()
+	return args.Get(0).([]*rpcmodels.Domain), args.Error(1)
+}
+
+func (s *mockedStore) GetMembers(datacenterId string) ([]*rpcmodels.Member, error) {
+	args := s.Called(datacenterId)
+	return args.Get(0).([]*rpcmodels.Member), args.Error(1)
+}
+
+func (s *mockedStore) GetPools() ([]*rpcmodels.Pool, error) {
+	args := s.Called()
+	return args.Get(0).([]*rpcmodels.Pool), args.Error(1)
+}
+
+func TestBuildsAS3Declaration(t *testing.T) {
 	assert := assert.New(t)
-	assert.Equal("foo", "foo", "that makes sense")
-	t.Log("Hello 👋")
+	store := new(mockedStore)
+	store.On("GetDatacenters").Return([]*rpcmodels.Datacenter{
+		{Id: "dc1", Name: "dc1"},
+		{Id: "dc2", Name: "dc2"},
+	}, nil)
+	store.On("GetMembers", "dc1").Return([]*rpcmodels.Member{
+		{Id: "member1", Address: "200.10.0.1", Port: 80},
+	}, nil)
+	store.On("GetMembers", "dc2").Return([]*rpcmodels.Member{}, nil)
+	b := as3DeclarationBuilder{store: store}
+	declaration, err := b.Build()
+	assert.Nil(err, "failed to build the declaration")
+	expected := as3.ADC{SchemaVersion: "3.22.0"}
+	tenant := as3.Tenant{}
+	application := as3.Application{Template: "shared"}
+	application.AddEntity("cc_andromeda_srv_member1_dc1", as3.GSLBServer{
+		Class:          "GSLB_Server",
+		ServerType:     "generic-host",
+		DataCenter:     as3.PointerGSLBDataCenter{BigIP: "/Common/dc1"},
+		Devices:        []as3.GSLBServerDevice{{Address: "200.10.0.1"}},
+		Monitors:       []as3.PointerGSLBMonitor{{BigIP: "/Common/tcp"}},
+		VirtualServers: []as3.GSLBVirtualServer{{Address: "200.10.0.1", Port: 80}},
+	})
+	tenant.AddApplication("Shared", application)
+	expected.AddTenant("Common", tenant)
+	assert.Equal(expected, declaration, "declaration built does not match expectation")
 }
